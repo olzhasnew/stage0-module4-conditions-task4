0b9fd37607b8b7538b159cbe0b38d5c91ef285c2
olzhasnew <olzhas.ualikhanov@gmail.com>
2023-09-28 21:13:28 +0600

    6th attempt

--- src/main/java/school/mjc/stage0/conditions/task4/TernaryPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task4/TernaryPrinter.java
@@ -2,7 +2,10 @@ package school.mjc.stage0.conditions.task4;
 
 public class TernaryPrinter {
     public void printWhichIsBigger(int first, int second) {
-        int max = (first > second || first == second) ? first : second;
-        System.out.println(max);
+        int max = (first > second || first == second) ? first:second;
+        if (max == first) {
+            System.out.println("first");
+        }else {System.out.println("second");
+        }
     }
 }
